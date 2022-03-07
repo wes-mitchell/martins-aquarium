a9d37b08ce2482ebdef15643d57fbc0d6374a483
wes-mitchell <wmdrums@gmail.com>
2022-03-07 13:41:44 -0600

    adds functions to sort fish by hierarchy. Exercise in using .filter

--- scripts/fish/FishData.js
+++ scripts/fish/FishData.js
@@ -1,10 +1,3 @@
-/*
- *  To get you started, here's some properties of Bart.
- *  You need to add more properties to complete this
- *  representation as an object. Then add all the other
- *  fish to the collection.
- */
-
 const fishCollection = [
   {            
     species: "Yellow Tang",
@@ -32,9 +25,48 @@ const fishCollection = [
     image: "shark.jpg",
     name: "Bruce",
     food: "Yellow Tang"
+  },
+  {
+    species: "Northern Pike",
+    length: 9,
+    location: "Rapid City, SD",
+    saltwater: false,
+    image: "northern-pike.jpg",
+    name: "Pikey",
+    food: "Carp"
+  },
+  {
+    species: "Walleye",
+    length: 13.5,
+    location: "St Paul, MN",
+    saltwater: false,
+    image: "Walleye.jpg",
+    name: "Walley",
+    food: "Algae"
   }
 ]
 
 export const getFish = () => {
   return fishCollection
 };
+
+export const getMostHolyFish = () => {
+  const holyFishArray = getFish().filter(fish => {
+    return fish.length % 3 === 0
+  })
+  return holyFishArray
+}
+
+export const getSoldierFish = () => {
+  const soldiersArray = getFish().filter(fish => {
+    return fish.length % 5 === 0 && fish.length % 3 !== 0
+  })
+  return soldiersArray
+}
+
+export const getUnworthy = () => {
+  const unworthyArray = getFish().filter(fish => {
+    return fish.length % 3 !== 0 && fish.length % 5 !== 0
+  })
+  return unworthyArray
+}
--- scripts/fish/FishList.js
+++ scripts/fish/FishList.js
@@ -4,25 +4,46 @@
 
 // TODO: Import `getFish` from the data module
 
-import { getFish } from "./FishData.js";
+import { getFish, getMostHolyFish, getSoldierFish, getUnworthy } from "./FishData.js";
 import { Fish } from "./Fish.js";
 
 export const FishList = () => {
 
   // Get a reference to the `<section class="fishList">` element
-  const contentElement = document.querySelector(".fishList")
-  const fishes = getFish()
+  // const contentElement = document.querySelector(".fishList")
+  const holyFishElement = document.querySelector(".holy-fish")  
+  const soldierFishElement = document.querySelector(".soldier-fish")
+  const unworthyFishElement = document.querySelector(".unworthy-fish")
+  // const fishes = getFish()
+  const holyFishes = getMostHolyFish()
+  const soldierFishes = getSoldierFish()
+  const unworthyFishes = getUnworthy()
 
+  let holyFishHTMLRep = '';
+  let soldierFishHTMLRep = '';
+  let unworthyFishHTMLRep = '';
 
-  let fishHTMLRepresentation = ""; 
+  // let fishHTMLRepresentation = ""; 
 
   // Loop over the array of fish
-  for (const fish of fishes) {
-  fishHTMLRepresentation += Fish(fish);
-}
+  for (const holyfish of holyFishes) {
+  holyFishHTMLRep += Fish(holyfish);
+  }
+  for (const soldierFish of soldierFishes) {
+    soldierFishHTMLRep += Fish(soldierFish);
+  }
+  for (const unworthyFish of unworthyFishes) {
+    unworthyFishHTMLRep += Fish(unworthyFish);
+  }
 
   // Add to the existing HTML in the content element
-  contentElement.innerHTML += `
-    ${fishHTMLRepresentation}
+  holyFishElement.innerHTML += `
+    ${holyFishHTMLRep}
+  `
+  soldierFishElement.innerHTML += `
+  ${soldierFishHTMLRep}
+  `
+  unworthyFishElement.innerHTML += `
+  ${unworthyFishHTMLRep}
   `
 }
